48484a92949dff067a0cdab7bfb0a0fdddafb921
angel222119 <https://github.com/angel222119/portafolio2.git>
2026-06-14 19:06:27 -0600

    Actualizacion diseño

--- app.py
+++ app.py
@@ -71,11 +71,7 @@ if opcion == "Inicio":
 
     col1, col2 = st.columns([1,2])
 
-    with col1:
-        st.image(
-            "imagen.png",
-            width=220
-        )
+    
 
     with col2:
         st.markdown("##  Información sobre mi")
